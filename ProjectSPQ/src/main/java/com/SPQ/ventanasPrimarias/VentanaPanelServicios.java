50bb9d2b54ced5ed162eb8338549aec40d2fbe9b
gutiaitor6 <gutiaitor6@gmail.com>
2021-04-06 21:30:48 +0200

    Servicios de forma dinamica

--- ProjectSPQ/src/main/java/com/SPQ/ventanasPrimarias/VentanaPanelServicios.java
+++ ProjectSPQ/src/main/java/com/SPQ/ventanasPrimarias/VentanaPanelServicios.java
@@ -1,5 +1,6 @@
 package com.SPQ.ventanasPrimarias;
 
+import java.awt.Color;
 import java.awt.Image;
 
 import javax.swing.ImageIcon;
@@ -9,7 +10,9 @@ import javax.swing.JPanel;
 public class VentanaPanelServicios extends JPanel{
 	
 	public VentanaPanelServicios(String textoNombre, String textoFoto, String textoDescripción, int ref, double textoPrecio, String textoCategoria, String textoContacto) {
-		
+		setBackground(Color.WHITE);
+		setLayout(null);
+
 		JLabel lblFoto = new JLabel(textoFoto);
 		lblFoto.setBounds(30, 11, 170, 124);
 		add(lblFoto);
@@ -21,25 +24,27 @@ public class VentanaPanelServicios extends JPanel{
 		lblNombre.setBounds(30, 146, 133, 13);
 		add(lblNombre);
 		
-		JLabel lblDescripción = new JLabel(textoDescripción);
-		lblDescripción.setBounds(30, 170, 119, 13);
-		add(lblDescripción);
-		
-		JLabel lblRef = new JLabel(Integer.toString(ref));
-		lblRef.setBounds(30, 194, 45, 13);
-		add(lblRef);
-		
-		JLabel lblPrecio = new JLabel(Double.toString(textoPrecio));
-		lblPrecio.setBounds(30, 194, 45, 13);
-		add(lblPrecio);
-		
 		JLabel lblCategoria = new JLabel(textoCategoria);
 		lblCategoria.setBounds(30, 170, 119, 13);
 		add(lblCategoria);
 		
 		JLabel lblContacto = new JLabel(textoContacto);
-		lblContacto.setBounds(30, 170, 119, 13);
+		lblContacto.setBounds(30, 194, 119, 13);
 		add(lblContacto);
+		
+		JLabel lblDescripción = new JLabel(textoDescripción);
+		lblDescripción.setBounds(30, 210, 119, 13);
+		add(lblDescripción);
+		
+		JLabel lblRef = new JLabel(Integer.toString(ref));
+		lblRef.setBounds(30, 230, 45, 13);
+		add(lblRef);
+		
+		JLabel lblPrecio = new JLabel(Double.toString(textoPrecio));
+		lblPrecio.setBounds(30, 210, 45, 13);
+		add(lblPrecio);
+		
+
 		
 		
         
