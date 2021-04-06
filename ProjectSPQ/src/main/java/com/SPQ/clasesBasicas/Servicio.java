e077b2e7bc5e2275e618dad4b21a2fbb855363b6
gutiaitor6 <gutiaitor6@gmail.com>
2021-04-06 16:51:33 +0200

    Servicio detalles

---
+++ ProjectSPQ/src/main/java/com/SPQ/clasesBasicas/Servicio.java
@@ -0,0 +1,88 @@
+package com.SPQ.clasesBasicas;
+
+public class Servicio {
+
+	private String nombre;
+	private String descripcion;
+	private int ref; //Referencia con id único
+	private double precio;
+	private String categoria;
+	private String contacto;
+	
+	public Servicio(String nombre, String descripcion, int ref, double precio, String categoria, String contacto) {
+		super();
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.ref = ref;
+		this.precio = precio;
+		this.categoria = categoria;
+		this.contacto = contacto;
+	}
+	
+	public Servicio() {
+		super();
+		this.nombre = "";
+		this.descripcion = "";
+		this.ref = 0;
+		this.precio = 0.0;
+		this.categoria = "";
+		this.contacto = "";
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public int getRef() {
+		return ref;
+	}
+
+	public void setRef(int ref) {
+		this.ref = ref;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public String getContacto() {
+		return contacto;
+	}
+
+	public void setContacto(String contacto) {
+		this.contacto = contacto;
+	}
+
+	@Override
+	public String toString() {
+		return "Servicio [nombre=" + nombre + ", descripcion=" + descripcion + ", ref=" + ref + ", precio=" + precio
+				+ ", categoria=" + categoria + ", contacto=" + contacto + "]";
+	}
+	
+	
+	
+}
