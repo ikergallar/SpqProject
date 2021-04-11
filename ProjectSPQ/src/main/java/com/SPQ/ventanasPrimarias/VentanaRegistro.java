30ffd2d5486ede77ce3bb91bbb3fe4d7de7b0d50
MrFahrenheit00 <55535512+MrFahrenheit00@users.noreply.github.com>
2021-04-11 22:56:12 +0200

    Ventana registro acabada

--- ProjectSPQ/src/main/java/com/SPQ/ventanasPrimarias/VentanaRegistro.java
+++ ProjectSPQ/src/main/java/com/SPQ/ventanasPrimarias/VentanaRegistro.java
@@ -37,6 +37,7 @@ import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.SwingConstants;
+import javax.swing.JComboBox;
 
 public class VentanaRegistro extends JDialog {
 
@@ -52,6 +53,7 @@ public class VentanaRegistro extends JDialog {
 	private JTextField texto_direccion;
 	private ButtonGroup sexo = new ButtonGroup();
 	final VentanaPrincipal principal = new VentanaPrincipal();
+	private JTextField textoRespuesta;
 	
 	
 	
@@ -81,94 +83,119 @@ public class VentanaRegistro extends JDialog {
 	 * Create the frame.
 	 */
 	public VentanaRegistro() {
+		setTitle("Hustle - Registro");
 		
 		
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 588, 401);
+		setBounds(100, 100, 513, 496);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(39, 45, 53));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel labelIntroducirDatos = new JLabel("Introduzca los siguientes datos:");
-		labelIntroducirDatos.setForeground(Color.WHITE);
-		labelIntroducirDatos.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		labelIntroducirDatos.setBounds(31, 54, 200, 33);
-		contentPane.add(labelIntroducirDatos);
-		
-		JLabel labelRegistrarNuevoUsuario = new JLabel("REGISTRA NUEVO USUARIO");
-		labelRegistrarNuevoUsuario.setForeground(Color.WHITE);
-		labelRegistrarNuevoUsuario.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		labelRegistrarNuevoUsuario.setBounds(31, 23, 200, 21);
-		contentPane.add(labelRegistrarNuevoUsuario);
-		
 		JLabel labelNombre = new JLabel("Nombre");
 		labelNombre.setForeground(Color.WHITE);
 		labelNombre.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		labelNombre.setBounds(31, 97, 70, 17);
+		labelNombre.setBounds(31, 48, 70, 17);
 		contentPane.add(labelNombre);
 		
 		textoNombre = new JTextField();
-		textoNombre.setBounds(101, 97, 118, 19);
+		textoNombre.setBounds(280, 154, 175, 19);
 		contentPane.add(textoNombre);
 		textoNombre.setColumns(10);
 		
 		JLabel lableApellido = new JLabel("Apellido");
 		lableApellido.setForeground(Color.WHITE);
 		lableApellido.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		lableApellido.setBounds(31, 137, 70, 17);
+		lableApellido.setBounds(280, 48, 70, 17);
 		contentPane.add(lableApellido);
 		
 		textoApellido = new JTextField();
 		textoApellido.setColumns(10);
-		textoApellido.setBounds(101, 137, 118, 19);
+		textoApellido.setBounds(31, 154, 175, 19);
 		contentPane.add(textoApellido);
 		
-		JLabel labelEmail = new JLabel("Correo electr\u00F3nico");
+		JLabel labelEmail = new JLabel("Correo");
 		labelEmail.setForeground(Color.WHITE);
 		labelEmail.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		labelEmail.setBounds(31, 181, 112, 17);
+		labelEmail.setBounds(31, 200, 112, 17);
 		contentPane.add(labelEmail);
 		
 		texto_email = new JTextField();
 		texto_email.setColumns(10);
-		texto_email.setBounds(156, 181, 169, 19);
+		texto_email.setBounds(280, 226, 175, 19);
 		contentPane.add(texto_email);
 		
 		JLabel labelContrasena = new JLabel("Contrase\u00F1a");
 		labelContrasena.setForeground(Color.WHITE);
 		labelContrasena.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		labelContrasena.setBounds(31, 223, 82, 17);
+		labelContrasena.setBounds(280, 126, 82, 17);
 		contentPane.add(labelContrasena);
 		
 		texto_contrasena = new JPasswordField();
-		texto_contrasena.setBounds(123, 223, 144, 19);
+		texto_contrasena.setBounds(280, 76, 173, 19);
 		contentPane.add(texto_contrasena);
 		
 		JLabel lblConfPass = new JLabel("Confirmar contraseña");
-		lblConfPass.setBounds(41, 279, 46, 14);
+		lblConfPass.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblConfPass.setForeground(new Color(255, 255, 255));
+		lblConfPass.setBounds(280, 201, 200, 14);
 		contentPane.add(lblConfPass);
 		
 		texto_confPass = new JPasswordField();
-		texto_confPass.setBounds(123, 276, 144, 20);
+		texto_confPass.setBounds(31, 228, 175, 20);
 		contentPane.add(texto_confPass);
 		
 		JLabel labelUsuario = new JLabel("Usuario");
 		labelUsuario.setForeground(Color.WHITE);
 		labelUsuario.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		labelUsuario.setBounds(260, 97, 70, 17);
+		labelUsuario.setBounds(31, 126, 70, 17);
 		contentPane.add(labelUsuario);
 		
 		texto_usuario = new JTextField();
 		texto_usuario.setColumns(10);
-		texto_usuario.setBounds(334, 97, 118, 19);
+		texto_usuario.setBounds(31, 76, 175, 19);
 		contentPane.add(texto_usuario);
 		
 		
 		
-		JButton btnRegistrar = new JButton("Finalizar registro");
-		btnRegistrar.setBounds(388, 303, 153, 33);
+		JButton btnRegistrar = new JButton("REGISTRAR");
+		btnRegistrar.setBackground(new Color(255, 0, 0));
+		btnRegistrar.setForeground(Color.WHITE);
+		btnRegistrar.setFont(new Font("Tahoma", Font.BOLD, 18));
+		btnRegistrar.setBounds(0, 385, 536, 72);
 		contentPane.add(btnRegistrar);
+		
+		ImageIcon ico4 = new ImageIcon("imagenes/fondos.jpg");
+		
+		JLabel labelPregunta = new JLabel("Pregunta recuperación");
+		labelPregunta.setForeground(Color.WHITE);
+		labelPregunta.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		labelPregunta.setBounds(31, 285, 150, 17);
+		contentPane.add(labelPregunta);
+		
+		final JComboBox comboPreguntas = new JComboBox();
+		comboPreguntas.addItem("-");
+		comboPreguntas.addItem("¿Nombre de tu madre?");
+		comboPreguntas.addItem("¿Nombre de tu padre?");
+		comboPreguntas.addItem("¿Ciudad natal?");
+		comboPreguntas.addItem("¿Fecha especial?");
+		comboPreguntas.addItem("¿Mascota de la infancia?");
+		comboPreguntas.setBounds(31, 313, 175, 22);
+		contentPane.add(comboPreguntas);
+		
+		JLabel labelRespuesta = new JLabel("Respuesta");
+		labelRespuesta.setForeground(Color.WHITE);
+		labelRespuesta.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		labelRespuesta.setBounds(280, 285, 150, 17);
+		contentPane.add(labelRespuesta);
+		
+		textoRespuesta = new JTextField();
+		textoRespuesta.setColumns(10);
+		textoRespuesta.setBounds(280, 314, 175, 19);
+		contentPane.add(textoRespuesta);
+		
 		btnRegistrar.addActionListener(new ActionListener() {
 
 			@Override
@@ -190,7 +217,7 @@ public class VentanaRegistro extends JDialog {
 				nombre = textoNombre.getText();
 				apellido = textoApellido.getText();
 				
-				if (nombreUsuario.equals("") || contrasenya.equals("") || email.equals("") || nombre.equals("")||apellido.equals("")) {
+				if (nombreUsuario.equals("") || contrasenya.equals("") || email.equals("") || nombre.equals("")||apellido.equals("") ||textoRespuesta.equals("")) {
 					JOptionPane.showMessageDialog(null, "Es necesario rellenar todos los campos", "Error", 0);
 					LogController.log ( Level.WARNING, "Es necesario rellenar todos los campos " + (new Date()),null);
 					
@@ -211,6 +238,8 @@ public class VentanaRegistro extends JDialog {
 									usuario.setMail(email);
 									usuario.setNombre(nombre);
 									usuario.setApellido(apellido);
+									usuario.setPalabraRecuperacion(textoRespuesta.getText());
+									usuario.setPreguntaRecuperacion(comboPreguntas.getSelectedItem().toString());
 																	
 									
 									try {
@@ -253,55 +282,6 @@ public class VentanaRegistro extends JDialog {
 				}
 			}
 			
-		});
-		
-		JLabel labelDisponerDeCuenta = new JLabel("\u00BFYa dispone de una cuenta?");
-		labelDisponerDeCuenta.setForeground(Color.WHITE);
-		labelDisponerDeCuenta.setBounds(270, 29, 161, 13);
-		contentPane.add(labelDisponerDeCuenta);
-		
-		
-		
-		JButton btnIniciarSesion = new JButton("Iniciar Sesion");
-		
-		
-		btnIniciarSesion.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				VentanaLogin inicio = new VentanaLogin();
-				//inicio.setVisible(true);
-				setVisible(false);
-			}
-		});
-		btnIniciarSesion.setBounds(441, 27, 117, 17);
-		contentPane.add(btnIniciarSesion);
-		
-		JLabel labelFondo = new JLabel("");
-		labelFondo.setHorizontalAlignment(SwingConstants.TRAILING);
-		labelFondo.setBounds(0, 0, 579, 372);
-		ImageIcon ico4 = new ImageIcon("imagenes/fondos.jpg");
-        ImageIcon img4 = new ImageIcon(ico4.getImage().getScaledInstance(labelFondo.getWidth(), labelFondo.getHeight(), Image.SCALE_SMOOTH));
-        labelFondo.setIcon(img4);
-		contentPane.add(labelFondo);
-		
-		final JCheckBox checkVerPass = new JCheckBox("Visualizar contraseñas");
-		checkVerPass.setBounds(70, 319, 180, 23);
-		getContentPane().add(checkVerPass);
-		checkVerPass.addActionListener(new ActionListener() {	
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if(checkVerPass.isSelected()) {
-					
-					texto_contrasena.setEchoChar((char)0);
-					texto_confPass.setEchoChar((char)0);
-					
-				}else {
-					
-					texto_contrasena.setEchoChar(('*'));
-					texto_confPass.setEchoChar((char)0);
-				}
-				
-			}
 		});
 		
 		
