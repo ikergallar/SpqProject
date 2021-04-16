cb3ea5e9cfde4fbaa7a43fa003c0486b6b7a6ef1
ikergallar <ikergallar@opendeusto.es>
2021-04-16 22:45:02 +0200

    pom

--- ProjectSPQ/src/main/java/com/SPQ/clasesBasicas/UsuarioConectado.java
+++ ProjectSPQ/src/main/java/com/SPQ/clasesBasicas/UsuarioConectado.java
@@ -3,7 +3,7 @@ package com.SPQ.clasesBasicas;
 public class UsuarioConectado {
 	
 	public static boolean isConnected(String nombre) {
-		//Aqui va la magia
+		//Aqui va la magdia
 		return true;
 	}
 
