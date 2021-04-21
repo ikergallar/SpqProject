c8d7e4913595c637f11527a2bb1abf89b3d7015d
MrFahrenheit00 <55535512+MrFahrenheit00@users.noreply.github.com>
2021-04-21 23:56:04 +0200

    labels par meter imagenes y titulos

--- src/main/java/com/SPQ/ventanasPrimarias/VentanaPrincipal.java
+++ src/main/java/com/SPQ/ventanasPrimarias/VentanaPrincipal.java
@@ -7,6 +7,7 @@ import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 import java.awt.GridLayout;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 
 public class VentanaPrincipal {
 
@@ -52,26 +53,52 @@ public class VentanaPrincipal {
 		JPanel panelSelecVentana = new JPanel();
 		panelSelecVentana.setBounds(0, 89, 231, 592);
 		frmHustle.getContentPane().add(panelSelecVentana);
-		panelSelecVentana.setLayout(new GridLayout(5, 1, 0, 0));
+		panelSelecVentana.setLayout(new GridLayout(4, 1, 0, 0));
 		
 		JPanel ventPerfil = new JPanel();
 		panelSelecVentana.add(ventPerfil);
 		
+		JLabel imgPerfil = new JLabel("");
+		ventPerfil.add(imgPerfil);
+		
+		JLabel lblPerfil = new JLabel("PERFIL");
+		ventPerfil.add(lblPerfil);
+		
 		JPanel ventServicios = new JPanel();
 		panelSelecVentana.add(ventServicios);
+		
+		JLabel imgServicios = new JLabel("");
+		ventServicios.add(imgServicios);
+		
+		JLabel lblServicios = new JLabel("SERVICIOS");
+		ventServicios.add(lblServicios);
 		
 		JPanel ventOferta = new JPanel();
 		panelSelecVentana.add(ventOferta);
 		
+		JLabel imgOferta = new JLabel("");
+		ventOferta.add(imgOferta);
+		
+		JLabel lblOferta = new JLabel("OFERTA");
+		ventOferta.add(lblOferta);
+		
 		JPanel ventChat = new JPanel();
 		panelSelecVentana.add(ventChat);
 		
-		JPanel ventMisServicios = new JPanel();
-		panelSelecVentana.add(ventMisServicios);
+		JLabel imgChat = new JLabel("");
+		ventChat.add(imgChat);
+		
+		JLabel lblChat = new JLabel("CHAT");
+		ventChat.add(lblChat);
 		
 		JPanel logoImg = new JPanel();
 		logoImg.setBounds(0, 0, 422, 89);
 		frmHustle.getContentPane().add(logoImg);
+		logoImg.setLayout(null);
+		
+		JLabel imgLogoHorizontal = new JLabel("New label");
+		imgLogoHorizontal.setBounds(10, 11, 402, 67);
+		logoImg.add(imgLogoHorizontal);
 		
 		JButton btnCerrarSesion = new JButton("Cerrar Sesion");
 		btnCerrarSesion.setBounds(938, 31, 89, 23);
