571d2d3a8789449b22abcb891618170f87aadfb7
ikergallar <ikergallar@opendeusto.es>
2021-04-21 21:23:19 +0200

    Inicio de filtro de anuncios por categoria

--- src/main/java/com/SPQ/dataBase/DBManager.java
+++ src/main/java/com/SPQ/dataBase/DBManager.java
@@ -20,48 +20,48 @@ public class DBManager {
     public static DBManager getInstance() throws DBException {
         if (instance == null) {
             instance = new DBManager();
-  //          instance.initializeData();
+            instance.initializeData();
         }
 
         return instance;
     }
 
-//   public void initializeData () throws DBException{
-//		
-//		pmf = JDOHelper.getPersistenceManagerFactory("datanucleus.properties");
-//		pm = pmf.getPersistenceManager();
-//		tx = pm.currentTransaction();
-//		
-//		try {
-//			tx.begin();
-//			Usuario usuario=new Usuario("Aitor", "Davila" , "aidav13", "pass123","aidav@gmail.com" , "6839283948" ,"Calle Ave del Paraiso 9, Barcelona","Buena zona","",null,"Dua","¿Como se llama mi gato?", null);
-//			pm.makePersistent(usuario);
-//			Usuario usuario1=new Usuario("Aitora", "Davilaa" , "aidav13a", "passa123","aidav@gamail.com" , "68392848" ,"Callse Ave del Paraiso 9, Barcselona","Buensa zona","",null,"Dusa","¿Comso se llama mi gato?", null);
-//			pm.makePersistent(usuario1);
-//			Usuario usuario2=new Usuario("Aitsor", "Davilaada" , "aidavsfa13", "pass1dsf23","aidav@fsgmail.com" , "68392833948" ,"Calle Ave del Pafaraiso 9, Barcafelona","Buena zfona","",null,"Dua","¿Como se llamsa mi gato?", null);
-//			pm.makePersistent(usuario2);
-//			Usuario usuario3=new Usuario("Aitofr", "Davilfa" , "aidsfav13", "passfs123","aidav@gmail.com" , "68393948" ,"Calle Ave del Pfaaraiso 9, Barcelafona","Bsduena zona","",null,"Dua","¿Como se llamsfa mi gato?", null);
-//			pm.makePersistent(usuario3);
-//			
-//			Anuncio anuncio=new Anuncio("Aitor", "Ofrdaezco servicio de fontaneria" ,60,"", Categoria.ALBAÑIL,true,usuario);
-//			pm.makePersistent(anuncio);
-//			Anuncio anuncio1=new Anuncio("Aiadtor", "Ofrezco servicio de fontaneria" ,40,"", Categoria.ALBAÑIL,false,usuario1);
-//			pm.makePersistent(anuncio1);
-//			Anuncio anuncio2=new Anuncio("sitor", "Ofrezdco servicio de fontaneria" ,20,"", Categoria.ALBAÑIL,true,usuario2);
-//			pm.makePersistent(anuncio2);
-//			Anuncio anuncio3=new Anuncio("Aitoddr", "Ofrezco saaervicio de fontaneria" ,90,"", Categoria.ALBAÑIL,true,usuario3);
-//			pm.makePersistent(anuncio3);
-//			
-//
-//			tx.commit();
-//			
-//		} finally {
-//			if (tx.isActive()) {
-//				tx.rollback();
-//			}
-//			pm.close();
-//		}
-//	}
+   public void initializeData () throws DBException{
+		
+		pmf = JDOHelper.getPersistenceManagerFactory("datanucleus.properties");
+		pm = pmf.getPersistenceManager();
+		tx = pm.currentTransaction();
+		
+		try {
+			tx.begin();
+			Usuario usuario=new Usuario("Aitor", "Davila" , "aidav13", "pass123","aidav@gmail.com" , "6839283948" ,"Calle Ave del Paraiso 9, Barcelona","Buena zona",null,"Dua","¿Como se llama mi gato?", null);
+			pm.makePersistent(usuario);
+			Usuario usuario1=new Usuario("Aitora", "Davilaa" , "aidav13a", "passa123","aidav@gamail.com" , "68392848" ,"Callse Ave del Paraiso 9, Barcselona","Buensa zona",null,"Dusa","¿Comso se llama mi gato?", null);
+			pm.makePersistent(usuario1);
+			Usuario usuario2=new Usuario("Aitsor", "Davilaada" , "aidavsfa13", "pass1dsf23","aidav@fsgmail.com" , "68392833948" ,"Calle Ave del Pafaraiso 9, Barcafelona","Buena zfona",null,"Dua","¿Como se llamsa mi gato?", null);
+			pm.makePersistent(usuario2);
+			Usuario usuario3=new Usuario("Aitofr", "Davilfa" , "aidsfav13", "passfs123","aidav@gmail.com" , "68393948" ,"Calle Ave del Pfaaraiso 9, Barcelafona","Bsduena zona",null,"Dua","¿Como se llamsfa mi gato?", null);
+			pm.makePersistent(usuario3);
+			
+			Anuncio anuncio=new Anuncio("Aitor", "Ofrdaezco servicio de fontaneria" ,60,"", Categoria.ALBAÑIL,true, "peter");
+			pm.makePersistent(anuncio);
+			Anuncio anuncio1=new Anuncio("Aiadtor", "Ofrezco servicio de fontaneria" ,40,"", Categoria.ALBAÑIL,false,"peter");
+			pm.makePersistent(anuncio1);
+			Anuncio anuncio2=new Anuncio("sitor", "Ofrezdco servicio de fontaneria" ,20,"", Categoria.ALBAÑIL,true,"peter");
+			pm.makePersistent(anuncio2);
+			Anuncio anuncio3=new Anuncio("Aitoddr", "Ofrezco saaervicio de fontaneria" ,90,"", Categoria.ALBAÑIL,true,"peter");
+			pm.makePersistent(anuncio3);
+			
+
+			tx.commit();
+			
+		} finally {
+			if (tx.isActive()) {
+				tx.rollback();
+			}
+			pm.close();
+		}
+	}
    
    public void crearAnuncio(Anuncio anuncio) throws DBException{
 		
@@ -235,6 +235,23 @@ public class DBManager {
 		Query<AnuncioGuardado> query = pm.newQuery("javax.jdo.query.SQL","Select * FROM anuncioguardado");
 		query.setClass(AnuncioGuardado.class);
 		List<AnuncioGuardado> results = query.executeList();
+		
+		tx.commit();
+		pm.close();
+		return results;
+		
+	}
+   	
+   	public List<Anuncio> filtroCategoria(Categoria categoria) throws DBException{
+		PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory("datanucleus.properties");
+		PersistenceManager pm = pmf.getPersistenceManager();
+		Transaction tx = pm.currentTransaction();
+		
+		tx.begin();
+		
+		Query<Anuncio> query = pm.newQuery("javax.jdo.query.SQL","select * from anuncio where categoria='" + categoria +"'");
+		query.setClass(Anuncio.class);
+		List<Anuncio> results = query.executeList();
 		
 		tx.commit();
 		pm.close();
--- src/main/java/com/SPQ/ventanasPrimarias/VentanaAnuncios.java
+++ src/main/java/com/SPQ/ventanasPrimarias/VentanaAnuncios.java
@@ -9,16 +9,22 @@ import javax.swing.JFrame;
 import javax.swing.JList;
 
 import com.SPQ.clasesBasicas.Anuncio;
+import com.SPQ.clasesBasicas.Categoria;
 import com.SPQ.dataBase.DBException;
 import com.SPQ.dataBase.DBManager;
 import com.SPQ.ventanasLogin.VentanaLogin;
 import javax.swing.JScrollPane;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+import javax.swing.JComboBox;
 
 public class VentanaAnuncios extends JFrame{
 	
 	private DefaultListModel modelo;
+	private DefaultListModel modeloCategoria;
 	private JScrollPane scrollPane;
 	private JList list;
+	private JList listaCategoria;
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -37,20 +43,47 @@ public class VentanaAnuncios extends JFrame{
 					
 		
 		list = new JList();
-		list.setBounds(66, 530, 339, -479);
+		list.setBounds(192, 155, 218, 365);
 		getContentPane().add(list);
 		
+		listaCategoria = new JList();
+		listaCategoria.setBounds(192, 155, 218, 365);
+		getContentPane().add(listaCategoria);
+		
 		DBManager conn = new DBManager();
+		
+		JRadioButton rdbtnCategoria = new JRadioButton("Categoría");
+		rdbtnCategoria.setBounds(57, 40, 109, 23);
+		getContentPane().add(rdbtnCategoria);
+		
+		JComboBox<Categoria> comboCategoria = new JComboBox<Categoria>();
+		comboCategoria.addItem(Categoria.INFORMATICO);
+		comboCategoria.addItem(Categoria.ALBAÑIL);
+        comboCategoria.addItem(Categoria.FONTANERO);      
+        comboCategoria.addItem(Categoria.PERSIANERO);
+		comboCategoria.setBounds(45, 70, 105, 22);
+		getContentPane().add(comboCategoria);
 		
 		modelo = new DefaultListModel();
 		modelo.addElement(conn.listarAnuncios());
 		list.setModel(modelo);
 		
-		scrollPane = new JScrollPane(list);
-		scrollPane.setBounds(47, 50, 411, 521);
+		modeloCategoria = new DefaultListModel();
+		modeloCategoria.addElement(conn.filtroCategoria((Categoria)comboCategoria.getSelectedItem()));
+		listaCategoria.setModel(modeloCategoria);
+		
+		scrollPane = new JScrollPane(listaCategoria);
+		scrollPane.setBounds(45, 106, 411, 521);
 		getContentPane().add(scrollPane);
 		
-		this.setSize(600,600);
+		JLabel flitro = new JLabel("Filtrar por:");
+		flitro.setBounds(62, 11, 104, 14);
+		getContentPane().add(flitro);
+		
+		
+		
+		
+		this.setSize(600,697);
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		this.setTitle("ANUNCIOS");
