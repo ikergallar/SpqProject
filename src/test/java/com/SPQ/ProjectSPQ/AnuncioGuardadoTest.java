3db8732699a1b9e0d2b44bc271b0d37b6a7ec12a
alegriaaitor <55583546+alegriaaitor@users.noreply.github.com>
2021-04-21 17:59:02 +0200

    test nueva clase(comentado)

---
+++ src/test/java/com/SPQ/ProjectSPQ/AnuncioGuardadoTest.java
@@ -0,0 +1,23 @@
+package com.SPQ.ProjectSPQ;
+
+import static org.junit.Assert.assertEquals;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.junit.Test;
+
+import com.SPQ.clasesBasicas.AnuncioGuardado;
+import com.SPQ.clasesBasicas.Categoria;
+
+public class AnuncioGuardadoTest {
+
+//	@Test
+//	public void testGetNombre() {
+//		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+//		String fecha = "11-11-2001";
+//		Date date = sdf.parse(fecha);
+//		AnuncioGuardado anuncio= new AnuncioGuardado("Aitor", "Ofrezco servicio de fontaneria" ,56.0,"", Categoria.ALBAÑIL,true, "gomezJuan", date);
+//		assertEquals(null, anuncio.getFecha());	
+//	}
+}
