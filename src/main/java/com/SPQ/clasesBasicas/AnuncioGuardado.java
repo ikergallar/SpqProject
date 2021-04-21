127653d71266a413ea4640d81d7a256103c82a80
alegriaaitor <55583546+alegriaaitor@users.noreply.github.com>
2021-04-21 18:04:11 +0200

    Anuncioguardado añadido a la bd

--- src/main/java/com/SPQ/clasesBasicas/AnuncioGuardado.java
+++ src/main/java/com/SPQ/clasesBasicas/AnuncioGuardado.java
@@ -2,6 +2,12 @@ package com.SPQ.clasesBasicas;
 
 import java.sql.Date;
 
+import javax.jdo.annotations.Inheritance;
+import javax.jdo.annotations.InheritanceStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+
+@PersistenceCapable
+@Inheritance(strategy=InheritanceStrategy.NEW_TABLE)
 public class AnuncioGuardado extends Anuncio{
 	private Date fecha;
 
