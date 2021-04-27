1b3bdaef0eb2220e80226b8002002add5b2447ea
MrFahrenheit00 <55535512+MrFahrenheit00@users.noreply.github.com>
2021-04-27 19:24:57 +0200

    Clase Servicios rediseñada y adaptada para añadir a clase principal

--- src/main/java/com/SPQ/ventanasAnuncio/VentanaAnuncios.java
+++ src/main/java/com/SPQ/ventanasAnuncio/VentanaAnuncios.java
@@ -14,7 +14,6 @@ import javax.swing.JOptionPane;
 
 import com.SPQ.clasesBasicas.Anuncio;
 import com.SPQ.clasesBasicas.Categoria;
-import com.SPQ.clasesBasicas.Usuario;
 import com.SPQ.dataBase.DBException;
 import com.SPQ.dataBase.DBManager;
 import com.SPQ.ventanasLogin.VentanaLogin;
@@ -24,6 +23,8 @@ import javax.swing.JRadioButton;
 import javax.swing.JComboBox;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.Color;
 
 public class VentanaAnuncios extends JFrame{
 	
@@ -32,75 +33,91 @@ public class VentanaAnuncios extends JFrame{
 	private DefaultListModel modeloPrecio;
 	private DefaultListModel modeloOferta;
 	private DefaultListModel modeloUsuario;
-	private List<Anuncio> listaAnuncios;
-	private List<Anuncio> listaCategoria;
-	private List<Anuncio> listaPrecio;
-	private List<Anuncio> listaOferta;
-	private List<Anuncio> listaUsuario;
 	private JScrollPane scrollPane;
 	private JList list;
 	private ButtonGroup buttonGroup;	
 	private JTextField textUsername;
-	
-	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaAnuncios window = new VentanaAnuncios();
+					window.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 	public VentanaAnuncios(){
+		getContentPane().setForeground(new Color(39, 45, 53));
 		getContentPane().setEnabled(false);
 		getContentPane().setLayout(null);
 					
 		
 		list = new JList();
-		list.setBounds(28, 123, 535, 524);
+		list.setBounds(205, 88, 584, 440);
 		getContentPane().add(list);
 				
 		DBManager conn = new DBManager();
 		
 		JRadioButton rdbtnCategoria = new JRadioButton("Categoría");
-		rdbtnCategoria.setBounds(57, 40, 109, 23);
+		rdbtnCategoria.setForeground(Color.WHITE);
+		rdbtnCategoria.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		rdbtnCategoria.setBounds(41, 123, 109, 23);
 		getContentPane().add(rdbtnCategoria);
 			
 		JComboBox<Categoria> comboCategoria = new JComboBox<Categoria>();
+		comboCategoria.setBackground(Color.RED);
 		comboCategoria.setEnabled(false);
 		comboCategoria.addItem(Categoria.INFORMATICO);
 		comboCategoria.addItem(Categoria.ALBAYIL);
         comboCategoria.addItem(Categoria.FONTANERO);      
         comboCategoria.addItem(Categoria.PERSIANERO);
-		comboCategoria.setBounds(45, 70, 105, 22);
+		comboCategoria.setBounds(41, 153, 133, 33);
 		getContentPane().add(comboCategoria);
 		
 		modelo = new DefaultListModel();
 		try {
-			listaAnuncios = conn.listarAnuncios();
+			modelo.addElement(conn.listarAnuncios());
 		} catch (DBException e2) {
 			// TODO Auto-generated catch block
 			e2.printStackTrace();
 		}
+		list.setModel(modelo);
 		
-		for (Anuncio anuncios : listaAnuncios) {
-			modelo.addElement(anuncios);
-			list.setModel(modelo);
-		}
-		
-		JLabel flitro = new JLabel("Filtrar por:");
-		flitro.setBounds(62, 11, 104, 14);
+		JLabel flitro = new JLabel("FILTRADO");
+		flitro.setForeground(Color.WHITE);
+		flitro.setFont(new Font("Tahoma", Font.BOLD, 14));
+		flitro.setBounds(22, 87, 104, 14);
 		getContentPane().add(flitro);
 		
 		JRadioButton rdbtnPrecio = new JRadioButton("Precio:");
-		rdbtnPrecio.setBounds(173, 40, 109, 23);
+		rdbtnPrecio.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		rdbtnPrecio.setForeground(Color.WHITE);
+		rdbtnPrecio.setBounds(41, 220, 109, 23);
 		getContentPane().add(rdbtnPrecio);
 		
 		JComboBox comboPrecio = new JComboBox();
+		comboPrecio.setForeground(Color.WHITE);
+		comboPrecio.setBackground(Color.RED);
+		comboPrecio.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		comboPrecio.setEnabled(false);
 		comboPrecio.addItem("Mayor precio");
 		comboPrecio.addItem("Menor precio");
-		comboPrecio.setBounds(173, 70, 109, 22);
+		comboPrecio.setBounds(41, 250, 133, 35);
 		getContentPane().add(comboPrecio);
 		
 		JRadioButton rdbtnOferta = new JRadioButton("Ofertas:");
-		rdbtnOferta.setBounds(306, 40, 109, 23);
+		rdbtnOferta.setForeground(Color.WHITE);
+		rdbtnOferta.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		rdbtnOferta.setBounds(41, 317, 109, 23);
 		getContentPane().add(rdbtnOferta);
 		
 		JRadioButton rdbtnUsuario = new JRadioButton("Nombre de usuario:");
-		rdbtnUsuario.setBounds(413, 40, 165, 23);
+		rdbtnUsuario.setForeground(Color.WHITE);
+		rdbtnUsuario.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		rdbtnUsuario.setBounds(41, 369, 165, 23);
 		getContentPane().add(rdbtnUsuario);
 		
 		buttonGroup = new ButtonGroup();
@@ -111,15 +128,24 @@ public class VentanaAnuncios extends JFrame{
 		
 		
 		textUsername = new JTextField();
+		textUsername.setForeground(Color.WHITE);
+		textUsername.setBackground(Color.RED);
+		textUsername.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		textUsername.setEnabled(false);
-		textUsername.setBounds(423, 71, 86, 20);
+		textUsername.setBounds(41, 399, 98, 35);
 		getContentPane().add(textUsername);
 		textUsername.setColumns(10);
 		
 		JButton btnBuscar = new JButton("New button");
 		btnBuscar.setEnabled(false);
-		btnBuscar.setBounds(509, 70, 24, 22);
+		btnBuscar.setBounds(139, 399, 35, 35);
 		getContentPane().add(btnBuscar);
+		
+		JLabel lblTiruloServicios = new JLabel("SERVICIOS");
+		lblTiruloServicios.setForeground(Color.WHITE);
+		lblTiruloServicios.setFont(new Font("Tahoma", Font.BOLD, 22));
+		lblTiruloServicios.setBounds(325, 29, 139, 33);
+		getContentPane().add(lblTiruloServicios);
 			
 		rdbtnCategoria.addActionListener((ActionListener) new ActionListener() {
 		public void actionPerformed(ActionEvent e){   
@@ -137,18 +163,14 @@ public class VentanaAnuncios extends JFrame{
 			public void actionPerformed(ActionEvent e){   
 
 				modeloCategoria = new DefaultListModel();
+				modelo = modeloCategoria;
 				try {
-					listaCategoria = (conn.filtroCategoria((Categoria)comboCategoria.getSelectedItem()));
+					modeloCategoria.addElement(conn.filtroCategoria((Categoria)comboCategoria.getSelectedItem()));
 				} catch (DBException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-				
-				for (Anuncio anuncios : listaCategoria) {
-					modelo = modeloCategoria;
-					modeloCategoria.addElement(anuncios);
-					list.setModel(modeloCategoria);
-				}
+				list.setModel(modeloCategoria);
 		    }
 			});
 		
@@ -168,33 +190,28 @@ public class VentanaAnuncios extends JFrame{
 			public void actionPerformed(ActionEvent e){
 				if(comboPrecio.getSelectedItem().toString().equals("Mayor precio")) {
 					modeloPrecio = new DefaultListModel();
+					modelo = modeloPrecio;
 					try {
-						listaPrecio = (conn.filtroPrecioMayor());
+						modeloPrecio.addElement(conn.filtroPrecioMayor());
 					} catch (DBException e1) {
 						// TODO Auto-generated catch block
 						e1.printStackTrace();
 					}
 					
-					for (Anuncio anuncios : listaPrecio) {
-						modelo = modeloPrecio;
-						modeloPrecio.addElement(anuncios);
-						list.setModel(modeloPrecio);
-					}
-									
+					list.setModel(modeloPrecio);
+					
 				}else if(comboPrecio.getSelectedItem().toString().equals("Menor precio")) {
 					modeloPrecio = new DefaultListModel();
 					modelo = modeloPrecio;
 					try {
-						listaPrecio = (conn.filtroPrecioMenor());
+						modeloPrecio.addElement(conn.filtroPrecioMenor());
 					} catch (DBException e1) {
 						// TODO Auto-generated catch block
 						e1.printStackTrace();
 					}
 					
-					for (Anuncio anuncios : listaPrecio) {
-						modeloPrecio.addElement(anuncios);
-						list.setModel(modeloPrecio);
-					}
+					list.setModel(modeloPrecio);
+
 				}					
 			}
 		});
@@ -210,16 +227,13 @@ public class VentanaAnuncios extends JFrame{
 		        	modeloOferta = new DefaultListModel();
 					modelo = modeloOferta;
 					try {
-						listaOferta = (conn.filtroOfertas());
+						modeloOferta.addElement(conn.filtroOfertas());
 					} catch (DBException e1) {
 						// TODO Auto-generated catch block
 						e1.printStackTrace();
 					}
 					
-					for (Anuncio anuncios : listaOferta) {
-						modeloOferta.addElement(anuncios);
-						list.setModel(modeloOferta);
-					}
+					list.setModel(modeloOferta);
 		            
 		        }
 		    }
@@ -239,26 +253,24 @@ public class VentanaAnuncios extends JFrame{
 		btnBuscar.addActionListener((ActionListener) new ActionListener() {
 			public void actionPerformed(ActionEvent e){   
 				String nomUsuario = textUsername.getText();
-				modeloUsuario = new DefaultListModel();
-				Usuario usuario;
-				try {
-					usuario = conn.seleccionarUsuario(nomUsuario);
-					listaUsuario = (conn.filtroUsuario(usuario));
 
+		        try {
+					if (conn.filtroUsuario(nomUsuario) != null){
+						modeloUsuario = new DefaultListModel();
+						modelo = modeloUsuario;
+						modeloUsuario.addElement(conn.filtroUsuario(nomUsuario));
+						list.setModel(modeloUsuario);	        	
+					}else{
+						 JOptionPane.showMessageDialog(null, "El usuario no existe", "Error", 0);
+					}
 				} catch (DBException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
-				}				
-
-				for (Anuncio anuncios : listaUsuario) {
-					modelo = modeloUsuario;
-					modeloUsuario.addElement(anuncios);
-					list.setModel(modeloUsuario);
 				}
 		    }
 			});
 		
-		this.setSize(600,697);
+		this.setSize(835,592);
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		this.setTitle("ANUNCIOS");
